920c898a4d881602dd980318261a0e1fa9f7ff36
mixerp4 <praki2047@gmail.com>
2015-01-02 16:34:00 +0545

    Scrud view created for - Counters - Currencies

---
+++ FrontEnd/MixERP.Net.FrontEnd/db/src/05. scrud-views/core/core.currency_scrud_view.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW core.currency_scrud_view
+AS
+SELECT 
+  currencies.currency_code, 
+  currencies.currency_symbol, 
+  currencies.currency_name, 
+  currencies.hundredth_name
+FROM 
+  core.currencies;
