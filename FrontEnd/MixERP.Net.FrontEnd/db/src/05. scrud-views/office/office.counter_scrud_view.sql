21286149ca8cbe00a6c71754705abac3ca6377b6
mixerp4 <praki2047@gmail.com>
2015-01-06 15:37:48 +0545

    Previous errors fixed - Scrud views format - Scrud resources spelling

--- FrontEnd/MixERP.Net.FrontEnd/db/src/05. scrud-views/office/office.counter_scrud_view.sql
+++ FrontEnd/MixERP.Net.FrontEnd/db/src/05. scrud-views/office/office.counter_scrud_view.sql
@@ -1,13 +1,13 @@
 ﻿CREATE VIEW office.counter_scrud_view
 AS
 SELECT
-office.counters.counter_id,
-office.stores.store_code,
-office.stores.store_name,
-office.cash_repositories.cash_repository_code,
-office.cash_repositories.cash_repository_name,
-office.counters.counter_code,
-office.counters.counter_name
+	office.counters.counter_id,
+	office.stores.store_code,
+	office.stores.store_name,
+	office.cash_repositories.cash_repository_code,
+	office.cash_repositories.cash_repository_name,
+	office.counters.counter_code,
+	office.counters.counter_name
 FROM office.counters
 INNER JOIN office.stores
 ON office.counters.store_id=office.stores.store_id
