f24cff6b2ab5b1fe04e2f2f5c046150bc3601986
surya <surya_luitel@yahoo.com>
2015-01-25 17:52:17 +0545

    Scrud view created for fiscal year.

---
+++ FrontEnd/MixERP.Net.FrontEnd/db/src/05. scrud-views/core/core.fiscal_year_scrud_view.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW core.fiscal_year_scrud_view
+AS
+SELECT 
+  core.fiscal_year.fiscal_year_code, 
+  core.fiscal_year.fiscal_year_name, 
+  core.fiscal_year.starts_from, 
+  core.fiscal_year.ends_on
+FROM 
+  core.fiscal_year;
