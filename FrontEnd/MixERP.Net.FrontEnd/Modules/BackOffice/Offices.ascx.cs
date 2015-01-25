739f4e1be3cd6911d9743f40b56ecdb6e5aca200
surya <surya_luitel@yahoo.com>
2015-01-25 17:03:00 +0545

    Scrud View created for office.

--- FrontEnd/MixERP.Net.FrontEnd/Modules/BackOffice/Offices.ascx.cs
+++ FrontEnd/MixERP.Net.FrontEnd/Modules/BackOffice/Offices.ascx.cs
@@ -38,7 +38,7 @@ namespace MixERP.Net.Core.Modules.BackOffice
                 scrud.TableSchema = "office";
                 scrud.Table = "offices";
                 scrud.ViewSchema = "office";
-                scrud.View = "offices";
+                scrud.View = "office_scrud_view";
 
                 scrud.DisplayFields = GetDisplayFields();
                 scrud.DisplayViews = GetDisplayViews();
