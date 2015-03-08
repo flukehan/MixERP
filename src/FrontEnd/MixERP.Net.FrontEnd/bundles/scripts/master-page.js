f0abbd140c98ef0b12c5f686af955d3997bca3ad
binodnp <binodnp@outlook.com>
2015-03-08 14:34:16 +0545

    Fix for issue #115 "Widgets Are Not Draggable".

--- src/FrontEnd/MixERP.Net.FrontEnd/bundles/scripts/master-page.js
+++ src/FrontEnd/MixERP.Net.FrontEnd/bundles/scripts/master-page.js
@@ -51086,26 +51086,10 @@ var closeItemSelector = function () {
 
 ///#source 1 1 /Scripts/mixerp/master-page/sortable.js
 ////Widget Support
-$('#sortable-container').sortable({ placeholder: "ui-state-highlight", helper: 'clone', handle: 'h2' });
 
-//$(document).ready(function () {
-//    var height = 0;
-//    $('#sortable-container').each(function () {
-//        var items = $(this).find(".widget");
-
-//        items.each(function () {
-//            if ($(this).height() > height) {
-//                height = $(this).height();
-//            }
-//        });
-
-//        var margin = 0;
-
-//        items.each(function () {
-//            $(this).find(".segment").css("height", height - margin + "px");
-//        });
-//    });
-//});
+$(document).ready(function() {
+    $('#sortable-container').sortable({ placeholder: "ui-state-highlight", helper: 'clone', handle: 'div' });
+});
 
 ///#source 1 1 /Scripts/mixerp/master-page/semantic.js
 $(document).ready(function () {
