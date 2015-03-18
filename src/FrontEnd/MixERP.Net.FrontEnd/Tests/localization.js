766d5822aa572e04191c6e1f52b3f3ed5119a6b1
amrit syangtan <amritsyangtan@yahoo.com>
2015-03-18 05:10:41 -0700

    Javascript test created for getFormattedNumber.

---
+++ src/FrontEnd/MixERP.Net.FrontEnd/Tests/localization.js
@@ -0,0 +1,57 @@
+﻿///<reference path="../bundles/scripts/master-page.min.js"/>
+QUnit.test("localization.js -> getFormattedNumber (Case 1)", function (assert) {
+    //Cultures: en-US, zh-CN, fil-PH, ja-JP, ms-MY
+    window.currencyDecimalPlaces = 2;
+    window.thousandSeparator = ",";
+    window.decimalSeparator = ".";
+
+    var expectedNumbers = ['100.00', '1,000.00', '1,000,000,000.00', '1,000,000,000,000,000.00'];
+    var numbers = ['100', '1000', '1000000000', '1000000000000000'];
+
+    $.each(numbers, function (i, v) {
+        var actual = getFormattedNumber(v);
+        var expected = expectedNumbers[i];
+
+        assert.equal(actual, expected, "The function returned " +
+            "expected formatted number \"" + expected + "\".");
+    });
+});
+
+QUnit.test("localization.js -> getFormattedNumber (Case 2)", function (assert) {
+    //Cultures: es-ES, pt-PT, nl-NL, de-DE, sv-SE, id_ID
+    window.currencyDecimalPlaces = 2;
+    window.thousandSeparator = ".";
+    window.decimalSeparator = ",";
+
+    var expectedNumbers = ['100,00', '1.000,00', '1.000.000.000,00', '1.000.000.000.000.000,00'];
+    var numbers = ['100', '1000', '1000000000', '1000000000000000'];
+
+
+    $.each(numbers, function (i, v) {
+        var actual = getFormattedNumber(v);
+        var expected = expectedNumbers[i];
+
+        assert.equal(actual, expected, "The function returned " +
+            "expected formatted number \"" + expected + "\".");
+    });
+});
+
+QUnit.test("localization.js -> getFormattedNumber (Case 3)", function (assert) {
+    //Cultures: fr-FR, ru-RU 
+    window.currencyDecimalPlaces = 2;
+    window.thousandSeparator = " ";
+    window.decimalSeparator = ",";
+
+    var expectedNumbers = ['100,00', '1 000,00', '1 000 000 000,00', '1 000 000 000 000 000,00'];
+    var numbers = ['100', '1000', '1000000000', '1000000000000000'];
+
+
+    $.each(numbers, function (i, v) {
+        var actual = getFormattedNumber(v);
+        var expected = expectedNumbers[i];
+
+        assert.equal(actual, expected, "The function returned " +
+            "expected formatted number \"" + expected + "\".");
+    });
+});
+
