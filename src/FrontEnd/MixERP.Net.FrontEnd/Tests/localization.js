14488aa130593c28ae03b5209804125dbd3f06a4
amrit syangtan <amritsyangtan@yahoo.com>
2015-03-18 05:13:24 -0700

    Javascript test created for parseFormattedNumber

--- src/FrontEnd/MixERP.Net.FrontEnd/Tests/localization.js
+++ src/FrontEnd/MixERP.Net.FrontEnd/Tests/localization.js
@@ -55,3 +55,43 @@ QUnit.test("localization.js -> getFormattedNumber (Case 3)", function (assert) {
     });
 });
 
+QUnit.test("localization.js -> parseFormattedNumber (Case 1)", function (assert) {
+    //Cultures: en-US, zh-CN, fil-PH, ja-JP, ms-MY
+    window.thousandSeparator = ".";
+    window.decimalSeparator = ",";
+    var num = '1,000,000,000,000.00';
+
+    var expected = '1000000000000.00';
+    var actual = parseFormattedNumber(num);
+
+  
+    assert.equal(actual,expected,"The function returned expected parsed formatted number \"" + expected + "\".");
+
+});
+
+QUnit.test("localization.js -> parseFormattedNumber (Case 2)", function (assert) {
+    //Cultures: es-ES, pt-PT, nl-NL, de-DE, sv-SE, id_ID
+    window.thousandSeparator = ".";
+    window.decimalSeparator = ",";
+    var num = '1.000.000.000.000,00';
+
+    var expected = '1000000000000.00';
+    var actual = parseFormattedNumber(num);
+
+  
+    assert.equal(actual,expected,"The function returned expected parsed formatted number \"" + expected + "\".");
+
+});
+QUnit.test("localization.js -> parseFormattedNumber (Case 3)", function (assert) {
+    //Cultures: fr-FR, ru-RU 
+    window.thousandSeparator = " ";
+    window.decimalSeparator = ",";
+    var num = '1 000 000 000 000,00';
+
+    var expected = '1000000000000.00';
+    var actual = parseFormattedNumber(num);
+
+  
+    assert.equal(actual,expected,"The function returned expected parsed formatted number \"" + expected + "\".");
+
+});
