1ff2356926e3138c1fd087407ac01def19f8f91f
Deepak Dahal <deepakdahalfloyd@gmail.com>
2015-06-29 15:52:54 +0545

    Proposed fix for #266 (Resource ScrudResource.maintained_by was not found (Bank Account). ).

---
+++ src/FrontEnd/MixERP.Net.FrontEnd/db/release-1/update-1/src/05.scrud-views/core/1.core.bank_account_scrud_view.sql
@@ -0,0 +1,21 @@
+DROP VIEW IF EXISTS core.bank_account_scrud_view CASCADE;
+
+CREATE OR REPLACE VIEW core.bank_account_scrud_view
+AS
+SELECT
+    account_id,
+    office.users.user_name AS maintained_by_user_id,
+    office.offices.office_code || ' (' || office.offices.office_name || ')' AS office,
+    bank_name,
+    bank_branch,
+    bank_contact_number,
+    bank_address,
+    bank_account_number,
+    bank_account_type,
+    relationship_officer_name,
+    is_merchant_account
+FROM core.bank_accounts
+INNER JOIN office.users
+ON core.bank_accounts.maintained_by_user_id = office.users.user_id
+INNER JOIN office.offices
+ON core.bank_accounts.office_id = office.offices.office_id;
